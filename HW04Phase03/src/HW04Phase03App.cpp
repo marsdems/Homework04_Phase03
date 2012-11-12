b88085282cfba99ddcf8beeab12aaadbb956a656
Mike Marsden <marsdems@muohio.edu>
2012-11-12 11:44:45 -0500

    Added small functionality

--- HW04Phase03/src/HW04Phase03App.cpp
+++ HW04Phase03/src/HW04Phase03App.cpp
@@ -5,6 +5,8 @@
 * File: HW04Phase03App.cpp
 **********************************************************/
 #include "cinder/app/AppBasic.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/ImageIo.h"
 #include "cinder/gl/gl.h"
 #include "Starbucks.h"
 #include "marsdemsStarbucks.h"
@@ -21,14 +23,22 @@ class HW04Phase03App : public AppBasic {
 
 	void setup();
 	void mouseDown( MouseEvent event );	
+	void keyDown (KeyEvent event);
 	Entry* getData();
 	void update();
 	void draw();
 	void prepareSettings(Settings* setttings);
+
+	marsdemsStarbucks* inputData;
 private:
 	static const int kAppWidth=512;
 	static const int kAppHeight=512;
 	static const int kTextureSize=1024;
+
+	int bgCounter;
+	float closestX, closestY;
+	Surface* area;
+	gl::Texture bgMap;
 };
 
 void HW04Phase03App::prepareSettings(Settings* settings) {
@@ -38,13 +48,20 @@ void HW04Phase03App::prepareSettings(Settings* settings) {
 
 void HW04Phase03App::setup()
 {
+	bgMap = gl::Texture(loadImage("../resources/Map.jpg"));
+	bgCounter = 0;
 	sizeCount = 0;
+
+	
 	Entry* inputFile = getData();	
 	int n = sizeCount;
 	marsdemsStarbucks* inputData = new marsdemsStarbucks();
 
 	//Testing Data for build() and getNearest()
 	inputData->build(inputFile, n);
+	delete [] inputFile;
+	sizeCount = 0;
+
 	Entry* nearest = inputData -> getNearest(.825088,.6786); //Looking for Yonkers, New York
 	
 	console() << nearest->identifier << "(" << nearest->x << ", " << nearest->y << ")" << endl;
@@ -89,6 +106,19 @@ Entry* HW04Phase03App::getData()
 
 void HW04Phase03App::mouseDown( MouseEvent event )
 {
+	double xLoc;
+	double yLoc;
+	
+	xLoc = event.getX()/kAppWidth;
+	yLoc = 1-1.0*event.getY()/kAppHeight;
+	Entry* closest = inputData->getNearest(xLoc, yLoc);
+	closestX = (kAppWidth*(closest->x));
+	closestY = kAppHeight - (kAppHeight*(closest->y));
+}
+
+void HW04Phase03App::keyDown(KeyEvent event)
+{
+	bgCounter++;
 }
 
 void HW04Phase03App::update()
@@ -97,10 +127,16 @@ void HW04Phase03App::update()
 
 void HW04Phase03App::draw()
 {
-	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
-	gl::color(ColorA(0,255,0,.75f));
-	gl::drawSolidCircle(Vec2f(100,100), 2, 0);
+	if (bgCounter % 2 == 0) {
+		gl::draw(bgMap, getWindowBounds());
+	}
+	else 
+		if (bgCounter % 2 == 1) {
+			gl::clear( Color( 0, 0, 0 ) ); 
+		}
+
+	gl::drawSolidCircle(Vec2f(closestX,closestY), 2, 0);
+
 }
 
 CINDER_APP_BASIC( HW04Phase03App, RendererGl )
